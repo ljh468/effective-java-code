8f2f695d160a250b0e96875dba841a541d242680
noah <ljh468@naver.com>
2023-09-24 17:54:11 +0900

    StackOverflowError 발생

--- src/main/java/org/effective/section02/item10/inheritance/SmellPointTest.java
+++ src/main/java/org/effective/section02/item10/inheritance/SmellPointTest.java
@@ -11,6 +11,8 @@ public class SmellPointTest {
     public static void main(String[] args) {
         SmellPoint p1 = new SmellPoint(1, 0, "sweat");
         ColorPoint p2 = new ColorPoint(1, 0, Color.RED);
-        p1.equals(p2);
+
+        // StackOverflowError 발생
+        // p1.equals(p2);
     }
 }
