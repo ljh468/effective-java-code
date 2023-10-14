bb3f4c5164a69414f936203295d1de153f5dfb61
noah <ljh468@naver.com>
2023-10-14 21:36:37 +0900

    item17 practice

---
+++ src/main/java/org/effective/section03/item17/memorymodel/Whiteship.java
@@ -0,0 +1,24 @@
+package org.effective.section03.item17.memorymodel;
+
+public class Whiteship {
+
+    private final int x;
+
+    private final int y;
+
+    public Whiteship() {
+        this.x = 1;
+        this.y = 2;
+    }
+
+    public static void main(String[] args) {
+        // JMM은 메모리 할당이 한쓰레드 안에서 유효한지 아닌지만 보장한다. (이론적인 이야기)
+        // 1. Object w = new Whiteship()
+        // 2. whiteship = w
+        // 3. w.x = 1
+        // 4. w.y = 2
+
+        // JMM final 규칙 -> final이 붙은 필드는 초기화가 된 이후에만 다른 쓰레드가 접근할 수 있다.
+        Whiteship whiteship = new Whiteship();
+    }
+}
---
+++ src/main/java/org/effective/section03/item17/part1/Person.java
@@ -0,0 +1,34 @@
+package org.effective.section03.item17.part1;
+
+public class Person {
+
+  // 가변 인스턴스 (가변 컴포넌트)
+  private final Address address;
+
+  public Person(Address address) {
+    this.address = address;
+  }
+
+  // 가변 인스턴스에 접근을 막아야 한다. (getter X)
+  public Address getAddress() {
+    Address copyOfAddress = new Address();
+    copyOfAddress.setZipCode(address.getZipCode());
+    copyOfAddress.setStreet(address.getStreet());
+    copyOfAddress.setCity(address.getCity());
+    return copyOfAddress;
+  }
+
+  public static void main(String[] args) {
+    // 최초의 Address는 시애틀
+    Address seattle = new Address();
+    seattle.setCity("Seattle");
+
+    // Address가 Redmond로 바뀔수 있다.
+    Person person = new Person(seattle);
+    Address redmond = person.getAddress();
+    redmond.setCity("Redmond");
+
+    System.out.println(person.address.getCity()); // Seattle
+    System.out.println(redmond.getCity()); // Redmond
+  }
+}
