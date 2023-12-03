33dd5eeb55e2e49a1c6950d251fd887345a841ba
noah <ljh468@naver.com>
2023-12-03 14:56:14 +0900

    item23 practice

--- src/main/java/org/effective/section04/item22/constantinterface/MyClass.java
+++ src/main/java/org/effective/section04/item22/constantinterface/MyClass.java
@@ -2,10 +2,10 @@ package org.effective.section04.item22.constantinterface;
 
 public class MyClass implements PhysicalConstants {
 
-    public static void main(String[] args) {
-        // 인터페이스는 타입을 정의하는 용도
-        System.out.println(BOLTZMANN_CONSTANT);
-        PhysicalConstants myClass = new MyClass(); // ?
-    }
+  public static void main(String[] args) {
+    // 인터페이스는 타입을 정의하는 용도
+    System.out.println(BOLTZMANN_CONSTANT);
+    PhysicalConstants myClass = new MyClass(); // ?
+  }
 
 }
