4369b8a469d845d31778acff454ed7d2fe67efd5
noah <ljh468@naver.com>
2023-12-03 13:58:06 +0900

    item22 practice

---
+++ src/main/java/org/effective/section04/item22/constantinterface/MyClass.java
@@ -0,0 +1,11 @@
+package org.effective.section04.item22.constantinterface;
+
+public class MyClass implements PhysicalConstants {
+
+    public static void main(String[] args) {
+        // 인터페이스는 타입을 정의하는 용도
+        System.out.println(BOLTZMANN_CONSTANT);
+        PhysicalConstants myClass = new MyClass(); // ?
+    }
+
+}
---
+++ src/main/java/org/effective/section05/item31/Main.java
@@ -0,0 +1,13 @@
+package org.effective.section05.item31;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+  public static void main(String[] args) {
+    Set<String> stringSet = new HashSet<>();
+    Set<Integer> integerSet = new HashSet<>();
+
+
+  }
+}
